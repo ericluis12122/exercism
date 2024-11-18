8da8b9d118db4b8727166d9efb88f5a6b38efd5b
ericluis12122 <erichernandez873@gmail.com>
2024-11-18 18:25:20 +0000

    up

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Solving programming challenges with the goal of having fun whileexploring and mastering the depths of various programming languages.
 
-## Score:
-1.### bash - 6 exercises
-2.### javascript - 28 exercises
+### Score:
+1. [bash](/bash) - 6 exercises
+2. [javascript](/javascript) - 28 exercises
--- update_score.sh
+++ update_score.sh
@@ -9,7 +9,7 @@ output_file="README.md"
 # Initialize the output file
 echo "Solving programming challenges with the goal of having fun whileexploring and mastering the depths of various programming languages." > "$output_file"
 echo "" >> "$output_file"
-echo "## Score:" >> "$output_file"
+echo "### Score:" >> "$output_file"
 
 # Position counter
 pos=1
@@ -21,7 +21,7 @@ for dir in */; do
         # Count the number of subdirectories
         subdirs=$(find "$dir" -mindepth 1 -maxdepth 1 -type d | wc -l)
         # Write the directory name and the number of subdirectories
-        echo "$pos.### ${dir%/} - $subdirs exercises" >> "$output_file"
+        echo "$pos. [${dir%/}](/${dir%/}) - $subdirs exercises" >> "$output_file"
         ((pos++))
     fi
 done
